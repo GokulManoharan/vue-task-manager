fcea4f54506320e6de188ee6fbf409733c9d6071
Gokul Manoharan <gokool2u@gmail.com>
2021-05-05 17:15:32 +0530

    initial commit vue project

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" 
+        :color="showAddTask ? 'red' : 'green'" 
+        :text="showAddTask ? 'Close task' :'Add task'"></Button>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default{
+    name: 'Header',
+    props: {
+        title:{
+            type: String,
+            default: "Hello"
+        },
+        showAddTask: Boolean
+    },
+    components: {
+        Button
+    },
+    computed: {
+        homePage() {
+            if(this.$route.path === "/"){
+                return true
+            }
+            return false
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3>Version 1.3.4</h3>
+    <router-link to="/">Go back</router-link>
+</template>
+
+<script>
+
+</script>
